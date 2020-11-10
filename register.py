1589f286c0eacfe5a1f99ca8292afbcffa3e9001
redbee99 <74188982+redbee99@users.noreply.github.com>
2020-11-10 16:23:11 +0900

    register.py
    templates 폴더 안에 있는 index.html 파일을 이용하여 정보를 POST로 입력 받아 mysql에 저장(등록) 시키는 API

---
+++ register.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import db_config
+from sqlalchemy import text, create_engine
+
+
+app = Flask(__name__)
+db = create_engine(db_config.DB_URL, encoding='utf-8', max_overflow = 0)
+
+
+@app.route('/')
+def test():
+    return render_template('index.html')
+
+
+@app.route('/post', methods=['POST'])
+def post():
+    params = {'c_name': request.form['c_name'],'email': request.form['email']}
+    new_user_id = db.execute(text("INSERT INTO inform (c_name, email) VALUES (:c_name, :email)"), params).lastrowid
+    return  "구독 성공!"
+
+
+if __name__ == '__main__':
+    app.run()
